cae676258cba4127e54a0e0373df3de08763a549
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 18:56:16 -0200

    Cap 3 - Pág 36

--- src/CDC/Exemplos/ConversorDeNumeroRomano.php
+++ src/CDC/Exemplos/ConversorDeNumeroRomano.php
@@ -5,12 +5,22 @@ namespace CDC\Exemplos;
 class ConversorDeNumeroRomano
 {
 
+    protected $tabela = array(
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000
+    );
+
     public function converte($numeroEmRomano)
     {
-        if ($numeroEmRomano === "V") {
-            return 5;
+        if (array_key_exists($numeroEmRomano, $this->tabela)) {
+            return $this->tabela[$numeroEmRomano];
         }
-        return 1;
+        return 0;
     }
 
 }
