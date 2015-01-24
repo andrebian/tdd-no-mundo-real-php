95dac7aefb8e5b9617293270ba7b76e54fed12b5
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 20:00:21 -0200

    Cap 5 - Pág 66

---
+++ src/CDC/Loja/Carrinho/MaiorPreco.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CDC\Loja\Produto;
+
+use CDC\Loja\Carrinho\CarrinhoDeCompras;
+
+class MaiorPreco
+{
+
+    public function encontra(CarrinhoDeCompras $carrinho)
+    {
+        return 0;
+    }
+
+}
---
+++ tests/CDC/Loja/Carrinho/MaiorPrecoTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CDC\Loja\Carrinho;
+
+use CDC\Loja\Test\TestCase,
+    CDC\Loja\Carrinho\CarrinhoDeCompras,
+    CDC\Loja\Carrinho\MaiorPreco;
+
+class MaiorPrecoTest extends TestCase
+{
+
+    public function testDeveRetornarZeroSeCarrinhoVazio()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $algoritmo = new MaiorPreco();
+        $valor = $algoritmo->encontra($carrinho);
+        $this->assertEquals(0, $valor, null, 0.0001);
+    }
+
+}
