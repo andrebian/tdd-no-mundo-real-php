bfcbe623a515601d03eb29a899a047dfc4169512
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 21:45:29 -0200

    Cap 7 - Pág 101 e 102

--- src/CDC/Loja/RH/CalculadoraDeSalario.php
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -10,20 +10,29 @@ class CalculadoraDeSalario
     public function calculaSalario(Funcionario $funcionario)
     {
         if ($funcionario->getCargo() === "desenvolvedor") {
-            if ($funcionario->getSalario() > 3000) {
-                return $funcionario->getSalario() * 0.8;
-            }
-            return $funcionario->getSalario() * 0.9;
-        } else if ($funcionario->getCargo() === "dba") {
-
-
-            if ($funcionario->getSalario() < 2500.0) {
-                return $funcionario->getSalario() * 0.85;
-            }
-            return $funcionario->getSalario() * 0.75;
+            return $this->dezOuVintePorCentoDeDesconto(
+                            $funcionario);
+        } else if ($funcionario->getCargo() === "dba" ||
+                $funcionario->getCargo() === "testador") {
+            return $this->quinzeOuVinteECincoPorCentoDeDesconto($funcionario);
         }
-
         throw new \Exception("Tipo de funcionário inválido");
+    }
+
+    private function dezOuVintePorCentoDeDesconto(Funcionario $funcionario)
+    {
+        if ($funcionario->getSalario() > 3000) {
+            return $funcionario->getSalario() * 0.8;
+        }
+        return $funcionario->getSalario() * 0.9;
+    }
+
+    private function quinzeOuVinteECincoPorCentoDeDesconto(Funcionario $funcionario)
+    {
+        if ($funcionario->getSalario() < 2500) {
+            return $funcionario->getSalario() * 0.85;
+        }
+        return $funcionario->getSalario() * 0.75;
     }
 
 }
