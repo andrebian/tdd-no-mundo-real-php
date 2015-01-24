35154fc1c7727756c4b75ee9b13ba348ecd625ea
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 19:43:55 -0200

    Cap 4 - Pág 50 e início da 51

---
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+use CDC\Loja\RH\Funcionario;
+
+class CalculadoraDeSalario
+{
+
+    public function calculaSalario(Funcionario $funcionario)
+    {
+        return 1350.0;
+    }
+
+}
