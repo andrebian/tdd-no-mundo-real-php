2cc71102a4e43023211afe2a2726c34eed01d54a
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 19:45:35 -0200

    Cap 4 - Pág 51

--- src/CDC/Loja/RH/CalculadoraDeSalario.php
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -9,6 +9,9 @@ class CalculadoraDeSalario
 
     public function calculaSalario(Funcionario $funcionario)
     {
+        if ($funcionario->getSalario() > 3000) {
+            return 3200.0;
+        }
         return 1350.0;
     }
 
--- tests/CDC/Loja/RH/CalculadoraDeSalarioTest.php
+++ tests/CDC/Loja/RH/CalculadoraDeSalarioTest.php
@@ -12,8 +12,7 @@ use CDC\Loja\RH\CalculadoraDeSalario,
 class CalculadoraDeSalarioTest extends PHPUnit
 {
 
-    public function
-    testCalculoSalarioDesenvolvedoresComSalarioAbaixoDoLimite()
+    public function testCalculoSalarioDesenvolvedoresComSalarioAbaixoDoLimite()
     {
         $calculadora = new CalculadoraDeSalario();
         $desenvolvedor = new Funcionario(
@@ -22,6 +21,16 @@ class CalculadoraDeSalarioTest extends PHPUnit
 
         $salario = $calculadora->calculaSalario($desenvolvedor);
         $this->assertEquals(1500.0 * 0.9, $salario, null, 0.00001);
+    }
+
+    public function testCalculoSalarioDesenvolvedoresComSalarioAcimaDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalario();
+        $desenvolvedor = new Funcionario(
+                "Andre", 4000.0, TabelaCargos::DESENVOLVEDOR);
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+        $this->assertEquals(4000.0 * 0.8, $salario, null, 0.00001);
     }
 
 }
