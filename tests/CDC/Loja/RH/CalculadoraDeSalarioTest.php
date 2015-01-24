613a322136f156978ceeab9cfb442563aaf8b1dc
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 19:36:27 -0200

    Cap 4 - Pág 50

---
+++ tests/CDC/Loja/RH/CalculadoraDeSalarioTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+require "./vendor/autoload.php";
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+use CDC\Loja\RH\CalculadoraDeSalario,
+    CDC\Loja\RH\Funcionario,
+    CDC\Loja\RH\TabelaCargos;
+
+class CalculadoraDeSalarioTest extends PHPUnit
+{
+
+    public function
+    testCalculoSalarioDesenvolvedoresComSalarioAbaixoDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalario();
+        $desenvolvedor = new Funcionario(
+                "Andre", 1500.0, TabelaCargos::DESENVOLVEDOR);
+
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+        $this->assertEquals(1500.0 * 0.9, $salario, null, 0.00001);
+    }
+
+}
