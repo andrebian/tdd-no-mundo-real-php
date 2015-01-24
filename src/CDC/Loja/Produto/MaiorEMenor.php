6eb8bf382da3373a7fb3621481e670d3a7b23456
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 18:46:27 -0200

    Cap 2 - Pág 24 e 25

---
+++ src/CDC/Loja/Produto/MaiorEMenor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CDC\Loja\Produto;
+
+use CDC\Loja\Carrinho\CarrinhoDeCompras;
+
+class MaiorEMenor
+{
+
+    private $menor;
+    private $maior;
+
+    public function encontra(CarrinhoDeCompras $carrinho)
+    {
+        foreach ($carrinho->getProdutos() as $produto) {
+
+            if (empty($this->menor) || $produto->getValor() < $this->menor->getValor()) {
+                $this->menor = $produto;
+            }
+            if (empty($this->maior) || $produto->getValor() > $this->maior->getValor()) {
+                $this->maior = $produto;
+            }
+        }
+    }
+
+    public function getMenor()
+    {
+        return $this->menor;
+    }
+
+    public function getMaior()
+    {
+        return $this->maior;
+    }
+
+}
