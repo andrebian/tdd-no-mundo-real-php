5ca5da94e9704682302ddd423b5c888d9d0ce4f9
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 20:23:42 -0200

    Cap 5 - Pág 71 e 72

--- src/CDC/Loja/Carrinho/CarrinhoDeCompras.php
+++ src/CDC/Loja/Carrinho/CarrinhoDeCompras.php
@@ -28,19 +28,17 @@ class CarrinhoDeCompras
 
     public function maiorValor()
     {
-        if (count($this->getItens()) === 0) {
+        if (count($this->getProdutos()) === 0) {
             return 0;
         }
 
-        $maiorValor = $this->getProdutos()[0]->getValor();
+        $maiorValor = $this->getProdutos()[0]->getValorUnitario();
         foreach ($this->getProdutos() as $produto) {
-            if ($maiorValor < $produto->getValor()) {
-                $maiorValor = $produto->getValor();
-
-
-                return $maiorValor;
+            if ($maiorValor < $produto->getValorUnitario()) {
+                $maiorValor = $produto->getValorUnitario();
             }
         }
+        return $maiorValor;
     }
 
 }
---
+++ tests/CDC/Loja/Carrinho/CarrinhoDeComprasTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CDC\Loja\Carrinho;
+
+use CDC\Loja\Test\TestCase,
+    CDC\Loja\Carrinho\CarrinhoDeCompras,
+    CDC\Loja\Produto\Produto;
+
+class CarrinhoDeComprasTest extends TestCase
+{
+
+    public function testDeveRetornarZeroSeCarrinhoVazio()
+    {
+        $carrinho = new CarrinhoDeCompras();
+        $valor = $carrinho->maiorValor();
+
+        $this->assertEquals(0, $valor, null, 0.0001);
+    }
+
+    public function testDeveRetornarValorDoItemSeCarrinhoCom1Elemento()
+    {
+        $carrinho = new CarrinhoDeCompras();
+        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+        $valor = $carrinho->maiorValor();
+
+        $this->assertEquals(900.00, $valor, null, 0.0001);
+    }
+
+    public function testDeveRetornarMaiorValorSeCarrinhoComMuitosElementos()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+        $carrinho->adiciona(new Produto("Fogão", 1500.00, 1));
+        $carrinho->adiciona(
+                new Produto("Máquina de lavar", 750.00, 1));
+        $valor = $carrinho->maiorValor();
+        $this->assertEquals(1500.00, $valor, null, 0.0001);
+    }
+
+}
