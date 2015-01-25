7a58b530196e402ad90196838d25380b6c36a596
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 22:21:39 -0200

    Cap 7 - Pág 107 e 108

--- src/CDC/Loja/RH/CalculadoraDeSalario.php
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -2,37 +2,17 @@
 
 namespace CDC\Loja\RH;
 
-use CDC\Loja\RH\Funcionario;
+use CDC\Loja\RH\Cargo,
+    CDC\Loja\RH\Funcionario;
 
 class CalculadoraDeSalario
 {
 
     public function calculaSalario(Funcionario $funcionario)
     {
-        if ($funcionario->getCargo() === "desenvolvedor") {
-            return $this->dezOuVintePorCentoDeDesconto(
-                            $funcionario);
-        } else if ($funcionario->getCargo() === "dba" ||
-                $funcionario->getCargo() === "testador") {
-            return $this->quinzeOuVinteECincoPorCentoDeDesconto($funcionario);
-        }
-        throw new \Exception("Tipo de funcionário inválido");
-    }
+        $cargo = new Cargo($funcionario->getCargo());
 
-    private function dezOuVintePorCentoDeDesconto(Funcionario $funcionario)
-    {
-        if ($funcionario->getSalario() > 3000) {
-            return $funcionario->getSalario() * 0.8;
-        }
-        return $funcionario->getSalario() * 0.9;
-    }
-
-    private function quinzeOuVinteECincoPorCentoDeDesconto(Funcionario $funcionario)
-    {
-        if ($funcionario->getSalario() < 2500) {
-            return $funcionario->getSalario() * 0.85;
-        }
-        return $funcionario->getSalario() * 0.75;
+        return $cargo->getRegra()->calcula($funcionario);
     }
 
 }
---
+++ src/CDC/Loja/RH/Cargo.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+class Cargo
+{
+
+    private $cargos = array(
+        "desenvolvedor" => "CDC\Loja\RH\DezOuVintePorCento",
+        "dba" => "CDC\Loja\RH\QuinzeOuVinteECincoPorCento",
+        "testador" => "CDC\Loja\RH\QuinzeOuVinteECincoPorCento"
+    );
+    private $regra;
+
+    public function __construct($regra)
+    {
+
+        if (array_key_exists($regra, $this->cargos)) {
+            $this->regra = $this->cargos[$regra];
+        } else {
+            throw new \Exception("Cargo inválido");
+        }
+    }
+
+    public function getRegra()
+    {
+        return new $this->regra();
+    }
+
+}
