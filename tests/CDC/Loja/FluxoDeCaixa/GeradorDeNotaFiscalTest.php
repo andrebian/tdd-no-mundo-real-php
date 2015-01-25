e2480e6fb1ab14f85222f1927fa9c2facf27511a
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-25 01:24:42 -0200

    Cap 8 - Pág 134 e 135

--- tests/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscalTest.php
+++ tests/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscalTest.php
@@ -52,4 +52,23 @@ class GeradorDeNotaFiscalTest extends TestCase
         $this->assertEquals(1000 * 0.94, $nf->getValor(), null, 0.00001);
     }
 
+    public function testDeveInvocarAcoesPosteriores()
+    {
+        $acao1 = Mockery::mock(
+                        "CDC\Loja\FluxoDeCaixa\AcaoAposGerarNotaInterface");
+        $acao1->shouldReceive("executa")->andReturn(true);
+        $acao2 = Mockery::mock(
+                        "CDC\Loja\FluxoDeCaixa\AcaoAposGerarNotaInterface");
+        $acao2->shouldReceive("executa")->andReturn(true);
+        $gerador = new GeradorDeNotaFiscal(array($acao1, $acao2));
+        $pedido = new Pedido("Andre", 1000, 1);
+        $nf = $gerador->gera($pedido);
+
+        $this->assertTrue($acao1->executa($nf));
+        $this->assertTrue($acao2->executa($nf));
+        $this->assertNotNull($nf);
+        $this->assertInstanceOf(
+                "CDC\Loja\FluxoDeCaixa\NotaFiscal", $nf);
+    }
+
 }
