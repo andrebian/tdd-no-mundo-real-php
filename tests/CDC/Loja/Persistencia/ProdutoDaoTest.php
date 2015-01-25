513bafa21fed066b6c0441cea0d2beae09a9d94e
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-25 13:18:54 -0200

    Cap 10 - Pág 163 e 164

--- tests/CDC/Loja/Persistencia/ProdutoDaoTest.php
+++ tests/CDC/Loja/Persistencia/ProdutoDaoTest.php
@@ -12,12 +12,21 @@ class ProdutoDaoTest extends TestCase
 
     public function testDeveAdicionarUmProduto()
     {
-        $conn = (new ConexaoComBancoDeDados())->getConexao();
-        $produtoDao = new ProdutoDao($conn);
+        $conexao = (new ConexaoComBancoDeDados())->getConexao();
+        $produtoDao = new ProdutoDao($conexao);
         $produto = new Produto("Geladeira", 150.0);
-        $produtoDao->adiciona($produto);
 
-        // como validar?
+        // Sobrescrevendo a conexão para continuar trabalhando
+        // sobre a mesma já instanciada
+        $conexao = $produtoDao->adiciona($produto);
+
+        // buscando pelo id para
+        // ver se está igual o produto do cenário
+        $salvo = $produtoDao->porId($conexao->lastInsertId());
+
+        $this->assertEquals($salvo["descricao"], $produto->getDescricao());
+        $this->assertEquals($salvo["valor_unitario"], $produto->getValorUnitario());
+        $this->assertEquals($salvo["status"], $produto->getStatus());
     }
 
 }
