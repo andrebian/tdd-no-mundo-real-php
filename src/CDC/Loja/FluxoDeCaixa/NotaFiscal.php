6ff6a608573f3e0867702ec18ef922ca5339b75e
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-24 23:34:49 -0200

    Cap 8 - Pág 116 e 117

---
+++ src/CDC/Loja/FluxoDeCaixa/NotaFiscal.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CDC\Loja\FluxoDeCaixa;
+
+class NotaFiscal
+{
+
+    private $cliente;
+    private $valor;
+    private $data;
+
+    public function __construct($cliente, $valor, $data)
+    {
+        $this->cliente = $cliente;
+        $this->valor = $valor;
+        $this->data = $data;
+    }
+
+    public function getCliente()
+    {
+        return $this->cliente;
+    }
+
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+}
---
+++ src/CDC/Loja/FluxoDeCaixa/Pedido.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CDC\Loja\FluxoDeCaixa;
+
+class Pedido
+{
+
+    private $cliente;
+    private $valorTotal;
+    private $quantidadeItens;
+
+    public function __construct($cliente, $valorTotal, $quantidadeItens)
+    {
+        $this->cliente = $cliente;
+        $this->valorTotal = $valorTotal;
+        $this->quantidadeItens = $quantidadeItens;
+    }
+
+    public function getCliente()
+    {
+        return $this->cliente;
+    }
+
+    public function getValorTotal()
+    {
+        return $this->valorTotal;
+    }
+
+    public function getQuantidadeItens()
+    {
+        return $this->quantidadeItens;
+    }
+
+}
