a56a9eb760afc3ccad765f93cd1944fb8ecf661a
Andre Cardoso <andrecardosodev@gmail.com>
2015-01-25 12:31:24 -0200

    Cap 9 - Pág 147 e 148

--- src/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletos.php
+++ src/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletos.php
@@ -12,10 +12,15 @@ class ProcessadorDeBoletos
 
     public function processa(ArrayObject $boletos, Fatura $fatura)
     {
+        $valorTotal = 0;
         $pagamentosFatura = $fatura->getPagamentos();
         foreach ($boletos as $boleto) {
             $pagamento = new Pagamento($boleto->getValor(), MeioPagamento::BOLETO);
             $pagamentosFatura->append($pagamento);
+        }
+        $valorTotal += $boleto->getValor();
+        if ($valorTotal >= $fatura->getValor()) {
+            $fatura->setPago(true);
         }
     }
 
--- tests/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletoTest.php
+++ tests/CDC/Loja/FluxoDeCaixa/ProcessadorDeBoletoTest.php
@@ -41,4 +41,14 @@ class ProcessadorDeBoletosTest extends TestCase
         $this->assertEquals(200.0, $valor2, null, 0.00001);
     }
 
+    public function testDeveMarcarFaturaComoPagoCasoBoletoUnicoPagueTudo()
+    {
+        $processador = new ProcessadorDeBoletos();
+        $fatura = new Fatura('Cliente', 150.0);
+        $boletos = new ArrayObject();
+        $boletos->append(new Boleto(150.0));
+        $processador->processa($boletos, $fatura);
+        $this->assertTrue($fatura->isPago());
+    }
+
 }
